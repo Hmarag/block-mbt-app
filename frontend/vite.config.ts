fe388f74d8ae0c1cf002a3e506ce42b0684daa88
Mosh Hamedani <hvmarag@gmail.com>
2025-10-27 20:33:28 +0200

    tester3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base : '/block-mbt-app/frontend',
+  base : '/Prekonias/block-mbt-app/frontend',
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
