798b9af1f38b70131858da052fecf4cfa0e96b40
Mosh Hamedani <hvmarag@gmail.com>
2025-10-27 19:57:39 +0200

    i dont know

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const basePath = process.env.VITE_BASE_PATH || '/'
+
 
 export default defineConfig({
-  base: basePath,
+  base : '/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
